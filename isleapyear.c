4b08f0447c29d13eba241a29bea9f7900d093b1e
trevormckaya <33607040+trevormckaya@users.noreply.github.com>
2026-05-26 08:19:45 -0700

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -6,16 +6,12 @@
 int main(int argc, char *argv[]) {
 
     int year = atoi(argv[1]);
-    scanf("%d", &year);
-
+    
     if (year % 4 == 0){
-        if (year % 100 != 0) {
+        if (year % 100 != 0 || year % 400 == 0) {
             printf("%d was a leap year",year);
             return 0;
-        } else if (year % 400 == 0) {
-            printf("%d was a leap year",year);
-            return 0;
-        }
+        } 
       } 
     printf("%d was not a leap year",year);
     return 0;
