bb900a9ac59e2e9c94699dc73bda911794c0c0af
trevormckaya <33607040+trevormckaya@users.noreply.github.com>
2026-06-06 10:05:03 -0700

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -99,8 +99,12 @@ struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queu
 {
     if (test_null_pcb(current_process)){
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
         new_process.remaining_bursttime = new_process.total_bursttime;
+        if (time_quantum < new_process.total_bursttime) {
+            new_process.execution_endtime = timestamp + time_quantum;
+        } else {
+            new_process.execution_endtime = timestamp + new_process.total_bursttime;
+        }
         return new_process;
     } else {
         new_process.execution_starttime = 0;
